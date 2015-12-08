1747ee7824f4689c41f33ba1e69c1897fbbc2c00
ys305751572 <305751572@qq.com>
2015-12-08 22:18:53 +0800

    11

--- src/main/resources/sql/yqss.sql
+++ src/main/resources/sql/yqss.sql
@@ -35,7 +35,7 @@ CREATE TABLE `users` (
   `status` int(2) default 1 comment '用户状态 1正常 2:禁用'
   `create_date` datetime comment '创建时间',
   
-  PRIMARY KEY (`adspublishId`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `users_bank_card`;
@@ -45,8 +45,9 @@ create table `users_bank_card` (
   `user_id` int(32) not null comment '所属用户',
   `card_no` varchar(30) not null comment '卡号',
   `is_default` int(2) default 1 comment '是否默认 1默认 2非默认',
-  `create_date` datetime comment '创建时间'
-)
+  `create_date` datetime comment '创建时间',
+   PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `message`;
 
@@ -56,8 +57,9 @@ create table `message` (
   `content` varchar(500) default '' comment '消息内容',
   `user_id` int(32) not null comment '所属用户',
   `is_list` int(2) default 1 comment '是否显示 1显示 2不显示',
-  `create_date` datetime comment '创建时间'
-)
+  `create_date` datetime comment '创建时间',
+   PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `product`;
 
@@ -68,8 +70,10 @@ create table `product` (
 	`content` varchar(500) default '' comment '商品描述',
 	
 	`is_list`int(2) default 1 comment '是否上架 1:上架 2:下架',
-	`create_date` datetime comment '创建时间'
-)
+	`create_date` datetime comment '创建时间',
+	
+	 PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `product_images`;
 
@@ -80,7 +84,22 @@ create table `product_images`(
 	`height` double default 0 comment '图片高度',
 	`width` double default 0 comment '图片宽度',
 	`small_url` varchar(200) default '' comment '缩略图',
-)
+	
+	 PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `product_comment`;
+
+create table `product_comment`(
+	`id` int(32) not null auto_increment,
+	`product_id` int(32) not null comment '商品ID',
+	`user_id` int(32) not null comment '评论用户ID',
+	`content` varchar (500) default '' comment '评论内容',
+	`score` int(2) default 5 comment '评论等级 1-5'
+	
+	`create_date` datatime comment '创建时间',
+	PRIMARY KEY (`id`)
+)NGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `orders`;
 
@@ -90,4 +109,59 @@ create table `orders`(
 	`product_id` int(32) not null comment '商品ID',
 	
 	``
+	PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `hr_message`;
+
+create table `hr_message` (
+	`id` int(32) not null auto_increment,
+	`title` varchar(100) not null default '' comment '人力信息标题',
+	`content` varchar(500) default '' comment '人力信息内容',
+	`salary_min` int(4) default 0 comment '工资下限',
+	`salary_max` int(4) default 0 comment '工资上线',
+	`tel` varchar(20) default '' comment '电话',
+	`mobile`varchar(20) default '' comment '手机',
+	`city_id` int(4) default 0 comment '城市ID',
+	`area_id` int(4) default 0 comment '区域ID',
+	`street_id` int(4) default 0 comment '街道ID',
+	`company` varchar(50) default '' comment '公司名字',
+	`position` varchar(50) default '' comment '职位',
+	`counts` int(2) default 1 comment '招聘人数',
+	`is_list` int(2) default 1 comment '是否发布 1:发布 2:不发布'
+	
+	`create_date` datetime comment '创建时间',
+	PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `collection`;
+
+create table `collection` (
+  `id` int(32) not null auto_increment,
+  `user_id` int(32) not null comment '用户ID',
+  `product_id` int(32) not null comment '收藏商品ID',
+  
+   PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `ads`;
+
+create table `ads` (
+
 )
+
+DROP TABLE IF EXISTS `helper`;
+
+create table `helper` (
+  `id` int(32) not null auto_increment,
+  `type_id` int(2) not null comment '问题类型ID',
+  `question` varchar(100) default '' comment '问题',
+  `answer` varchar(500) default '' comment '答案',
+  
+  `sys_user_id` int(32) not null comment '',
+  `create_date` datatime comment '创建时间'
+  
+   PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
